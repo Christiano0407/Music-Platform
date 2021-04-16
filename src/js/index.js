4cafa49e7c5ae32c6e320142961bcf274b9a46ed
Christiano0407 <placebo0704@gmail.com>
2021-04-16 10:09:53 -0500

    song-js

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+// ====== ONE-PROGRESS 7 MUSIC--CONTAINER.
+
+
+
+//====== SONG-CONTAINER ======
+const songContainer = document.getElementById(`song-container`); 
+const playBtn = document.getElementById(`play`); 
+const prevBtn = document.getElementById(`prev`); 
+const nextBtn = document.getElementById(`next`); 
+
+const audio = document.getElementById(`audio`); 
+
+const songProgress = document.getElementById(`song-progress`); 
+const progressContainer = document.getElementById(`song-progress_container`); 
+
+const songTitle = document.getElementById(`song-title`); 
+const cover = document.getElementById(`cover`); 
+
+// Song Titles
+
