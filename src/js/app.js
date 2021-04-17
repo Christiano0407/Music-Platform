a4bcaae075d6c47639268e9ac712e9bbc5514c0e
Christiano0407 <placebo0704@gmail.com>
2021-04-17 10:42:30 -0500

    change-song

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,18 @@
-const musicContainer = document.getElementById(`music-container`); 
+const musicContainer = document.getElementById("music-container"); 
 const playBtn = document.getElementById(`play`); 
 const prevBtn = document.getElementById(`prev`); 
 const nextBtn = document.getElementById(`next`); 
 
-const audio = document.getElementById(`audio`); 
+const audio = document.getElementById("audio"); 
 
-const progress = document.getElementById(`progress`); 
-const progressContainer =  document.getElementById(`progress-container`); 
+const progress = document.getElementById("progress"); 
+const progressContainer =  document.getElementById("progress-container"); 
 
-const title = document.getElementById(`title`); 
-const cover = document.getElementById(`cover`); 
+const title = document.getElementById("title"); 
+const cover = document.getElementById("cover"); 
 
 // SONG TITLES
-const songs = [`hey`, `summer`, `ukelele`]
+const songs = ["hey", "summer", "ukulele"]
 
 // KEEP TRACK OF SONG
 let songIndex = 2
